0a2aaf23d768e4b889c652cd3eddc118a971fc7a
Danniel30 <87454878+Danniel30@users.noreply.github.com>
2022-06-17 16:21:03 -0400

    Avançando nos estudos

---
+++ secao3-python-intermediario/aula24/mapeamento.py
@@ -0,0 +1,32 @@
+from dados import produtos, pessoas, lista
+
+# nova_lista = map(lambda x: x * 2, lista)
+# nova_lista = [x * 2 for x in lista]
+# print(lista)
+# print(list(nova_lista))
+
+
+def aumenta_preco(p):
+    p['preco'] = round(p['preco'] * 1.05, 2)
+    return p
+
+
+def aumenta_idade(p):
+    p['nova_idade'] = round(p['idade'] * 1.20)
+    return p
+
+novos_produtos = map(aumenta_preco, produtos)
+
+
+for produto in novos_produtos:
+    print(produto)
+
+print()
+
+
+nomes = map(aumenta_idade, pessoas)
+
+
+for pessoa in nomes:
+    print(pessoa)
+
